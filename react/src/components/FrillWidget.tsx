408a6bd31be1d106cda8772d490e054aefae79ce
Mitchell Heddles <mitchell.heddles@gmail.com>
2021-11-30 11:01:56 -0500

    Added react class component example

--- react/src/components/FrillWidget.tsx
+++ react/src/components/FrillWidget.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const FrillWidget: React.FC = React.memo(() => {
+  // This effect will run once when the component has mounted
   React.useEffect(() => {
     // We need to keep a reference to the widget instance so we can cleanup
     // when the component unmounts
@@ -10,30 +11,33 @@ const FrillWidget: React.FC = React.memo(() => {
     const config: FrillConfig = {
       key: 'e0ceb593-2c29-48a6-9d66-78bca8008a4f', // <-- Add Widget key here
       callbacks: {
-        // This will be called once the widget is ready. We have to use this incase the
-        // Frill api is not available on thw window yet
+        // This will be called when the widget is loaded
         onReady: (frillWidget) => {
           widget = frillWidget;
         },
       },
     };
 
-    // Push the config to the list so the Frill api can auto-init the widget once it's loaded.
-    // When the api loads, it will create all widgets in the Frill_Config list and dispatch the
-    // config.callbacks.onReady event for each
-    window.Frill_Config = window.Frill_Config || [];
-    window.Frill_Config.push(config);
-
+    // Let's check if the Frill script has already loaded
     if ('Frill' in window) {
-      // If the Frill api is already available we can create the widget
+      // If the Frill api is already available we can create the widget now
       widget = window.Frill.widget(config);
+    } else {
+      // If the Frill api hasn't been loaded, we need to add our config to the list.
+      // When the api loads, it will create all widgets in the Frill_Config list and dispatch the
+      // config.callbacks.onReady event for each
+      window.Frill_Config = window.Frill_Config || [];
+      window.Frill_Config.push(config);
     }
 
     // Return a cleanup method so we can remove the widget when the component unmounts
     return () => {
-      // Destroy the widget instance. This will remove all event listeners and nodes added
-      // to the DOM by the widget
-      widget?.destroy();
+      // Check if there is an active widget
+      if (widget) {
+        // If there is a widget, destroy it, this will remove all event listeners and nodes added
+        // to the DOM by the widget
+        widget.destroy();
+      }
       // We also need to remove our config from the list so it doesn't get initialised.
       // This would only happen if the had component mounted/unmounted before the Frill api
       // had a chance to load.
@@ -41,8 +45,11 @@ const FrillWidget: React.FC = React.memo(() => {
     };
   }, []);
 
+  // We are going to render a button that will be used to open the widget. This is because
+  // the example widget is using a "CSS selector" for the "Launcher".
+  // If your widget does not use this launcher, you can return null.
   return (
-    <button type="button" className="frill-container btn">
+    <button type='button' className='frill-container btn'>
       Click here to show the Widget
     </button>
   );
