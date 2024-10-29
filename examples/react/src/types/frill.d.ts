a564dc7984cd193a0f1fff7c46252378b2d1da93
Mitchell Heddles <mitchell.heddles@gmail.com>
2024-10-29 11:47:23 -0600

    Updated types

--- examples/react/src/types/frill.d.ts
+++ examples/react/src/types/frill.d.ts
@@ -1,19 +1,56 @@
 declare global {
   interface Window {
     Frill: FrillContainerApi;
-    Frill_Containers: Record<string, FrillContainer>;
   }
 }
 
 /**
- * Widget
+ * A user identity
  */
+export interface GuestUser {
+  name: string;
+  email: string;
+  avatar?: string;
+}
+
+export interface RegisteredUser {
+  idx: string;
+  name: string;
+  email: string;
+  avatar?: string;
+  meta?: UserMeta;
+}
+
+export interface FrillUserTokens {
+  idx?: string;
+  isAnonymous: boolean;
+  frillToken: string;
+  frillRefreshToken: string;
+}
+
+export interface FrillIdentity {
+  tokens?: FrillUserTokens & {
+    scope: FrillAcessTokenScope;
+  };
+  userMeta: UserMeta;
+}
+
+export type FrillIdentityWithTokens = Required<FrillIdentity>;
+
+export type FrillAcessTokenScope = 'basic' | 'frill';
+
+export interface SSOUserToken {
+  ssoToken: string;
+}
+
+export type UserIdentificationInput = GuestUser | FrillUserTokens | SSOUserToken;
+
 export interface FrillWidget {
   open(): void;
   close(): void;
   toggle(): void;
   destroy(): void;
-  identify(user: WidgetGuestUser): void;
+  identify(user: UserIdentificationInput): void;
   unidentify(): void;
   updateSettings(settings: WidgetSettings, replace?: boolean): void;
   createIdea(defaultValues?: CreateIdeaDefaultValues): void;
@@ -22,8 +59,19 @@ export interface FrillWidget {
   setBadgeCount(count: number): void;
 }
 
+export interface FrillSurvey {
+  open(): void;
+  close(): void;
+  toggle(): void;
+  destroy(): void;
+  identify(user: UserIdentificationInput): void;
+  unidentify(): void;
+  type: SurveyType;
+}
+
+export type SurveyType = 'nps' | 'csat' | 'idea-poll' | 'open-feedback' | 'poll';
+
 export interface FrillContainer {
-  widgets: Record<string, FrillWidget>;
   destroy(): void;
 }
 
@@ -31,7 +79,10 @@ export type CancelablePromise<T> = Promise<T> & { destroy(): void };
 
 export type FrillApiInit = {
   (method: 'widget', config: WidgetConfiguration): CancelablePromise<FrillWidget>;
-  (method: 'container', config: WidgetContainerConfiguration): CancelablePromise<FrillContainer>;
+  (method: 'survey', config: SurveyConfiguration): CancelablePromise<FrillSurvey>;
+  (method: 'container', config: ContainerConfiguration): CancelablePromise<FrillContainer>;
+  (method: 'identify', input: FrillIdentityWithTokens | UserIdentificationInput): Promise<void>;
+  (method: 'unidentify'): Promise<void>;
 };
 
 export type FrillContainerApiQueue = Record<
@@ -42,6 +93,8 @@ export type FrillContainerApiQueue = Record<
 export type FrillContainerApi = FrillApiInit & {
   q: FrillContainerApiQueue;
   containers: Record<string, FrillContainer>;
+  surveys: Record<string, FrillSurvey>;
+  widgets: Record<string, FrillWidget>;
 };
 
 export type WidgetConfigurations = WidgetConfiguration | WidgetConfiguration[];
@@ -139,59 +192,113 @@ export interface WidgetContainerCallbacks {
   onDestroy?(): void;
 }
 
-export interface WidgetCallbacks {
-  /**
-   * Fired when widget is ready and announcements have loaded
-   */
-  onReady?(widget: FrillWidget): void;
-  /**
-   * Fired after the widget has been opened
-   */
-  onOpen?(): void;
-  /**
-   * Fired after the widget has been closed
-   */
-  onClose?(): void;
-  /**
-   * Fired when the widget is destroyed
-   */
-  onDestroy?(): void;
+export type WidgetSettings = any;
+
+export interface WidgetCallbacks extends FrameCallbacks<FrillWidget> {
   /**
    * Fired after the badge count changes
    */
-  onBadgeCount?(event: { announcements: any[]; count: number }): void;
+  onBadgeCount(event: {
+    announcements: Pick<RestAPI.AnnouncementArticle, 'idx' | 'slug' | 'published_at'>[];
+    count: number;
+  }): void;
   /**
    * Fired after a boosted announcement notification has been opened
    */
-  onNotificationOpen?(): void;
+  onNotificationOpen(): void;
   /**
    * Fired after a boosted announcement notification has been closed
    */
-  onNotificationClose?(): void;
+  onNotificationClose(): void;
+}
+
+/**
+ * The configuration that is used to initialize the survey
+ */
+export interface SurveyConfiguration {
+  /**
+   * Your survey key
+   * @required
+   */
+  key: string;
+  /**
+   * A unique identifier for you survey, used to create multiple instances of the same survey
+   * Defaults to the survey key
+   */
+  id?: string;
+  /**
+   * Your SSO (JWT) Frill token, used to authenticate users inside the survey
+   */
+  ssoToken?: string;
+  /**
+   * Frill user tokens
+   */
+  frillTokens?: FrillUserTokens;
+  /**
+   * Guest user identification
+   */
+  user?: GuestUser;
+  /**
+   * Optional callback functions for survey lifecycle events
+   */
+  callbacks?: Partial<SurveyCallbacks>;
+  /**
+   * Container node where the survey will be rendered
+   * @default document.body
+   */
+  container?: HTMLElement;
+  /**
+   * Load CSS styles
+   * @default true
+   */
+  loadStyles?: boolean;
+  /**
+   * Disable analytics
+   * @default false
+   */
+  disableAnalytics?: boolean;
+  /**
+   * Disable cookies
+   * @default false
+   */
+  disableCookies?: boolean;
+  /**
+   * When true survey submissions will not be saved
+   */
+  previewMode?: boolean;
+  /**
+   * Survey settings override
+   */
+  settings?: DeepPartial<SurveySettings>;
+}
+
+export type SurveySettings = any;
+
+export interface SurveyCallbacks extends FrameCallbacks<FrillSurvey> {}
+
+export type FrameCallbacks<TApp> = {
+  /**
+   * Fired when app is ready and announcements have loaded
+   */
+  onReady(app: TApp): void;
+  /**
+   * Fired after the app has been opened
+   */
+  onOpen(): void;
+  /**
+   * Fired after the app has been closed
+   */
+  onClose(): void;
+  /**
+   * Fired when the app is destroyed
+   */
+  onDestroy(): void;
   /**
    * Fired when a user logs in
    */
-  onLogin?(event: { user: WidgetUser }): void;
+  onLogin(event: { user: RegisteredUser }): void;
   /**
    * Fired when a user logs out
    */
-  onLogout?(): void;
-}
-
-export type WidgetSettings = any;
-
-/**
- * A widget user identity
- */
-export interface WidgetGuestUser {
-  name: string;
-  email: string;
-  avatar?: string;
-}
-
-export interface WidgetUser {
-  idx: string;
-  name: string;
-  email: string;
-  avatar?: string;
-}
+  onLogout(): void;
+};
